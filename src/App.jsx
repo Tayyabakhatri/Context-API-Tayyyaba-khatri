6562ddc833c57e1051cd0c37887a7bc6ed2ef490
Tayyabakhatri <Tayyabakhatri225@gmail.com>
2025-01-21 09:10:13 +0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
-
 import "./App.css";
 import Component1 from "./Component/Component1.jsx";
 import Component2 from "./Component/Component2.jsx";
 
-
-
 function App() {
   return (
     <>
-    
-       
-       <Component1 />
-       <Component2/>
-      
-     
+      <Component1 />
+      <Component2 />
     </>
   );
 }
